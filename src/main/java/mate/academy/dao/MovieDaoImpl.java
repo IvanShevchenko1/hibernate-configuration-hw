871b1c97915551d5e27fa2210cebaf7923a12c25
Ivanov <ivanshevchenko181@gmail.com>
2025-08-03 15:22:18 +0300

    Fixed an issue 3

--- src/main/java/mate/academy/dao/MovieDaoImpl.java
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -37,7 +37,7 @@ public class MovieDaoImpl implements MovieDao {
     @Override
     public Optional<Movie> get(Long id) {
         SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-        try(Session session = sessionFactory.openSession()) {
+        try (Session session = sessionFactory.openSession()) {
             return Optional.ofNullable(session.get(Movie.class, id));
         } catch (Exception e) {
             throw new DataProcessingException("Failed to get Movie from the DB");
